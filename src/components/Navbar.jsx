a7456025128f9168bf2314e33d4fa0b9c87c9338
RaiYashPramod <raiyashpramod16@gmail.com>
2023-09-30 10:35:48 +0530

    Mobile view navbar bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,25 +9,41 @@ const Navbar = () => {
     localStorage.removeItem("token");
   };
 
+  const closeMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg text-white bg-black font-mono h-auto p-8">
-        <div className="flex flex-row justify-between">
+      <nav
+        className="navbar navbar-expand-lg text-white bg-black font-mono h-auto p-8"
+        role="navigation"
+      >
+        <div className="flex flex-row justify-between items-center">
           <h3 className="text-white font-mono text-xl">
-            <Link to="/">PowrLingo</Link>
+            <Link to="/" aria-label="Home">
+              PowrLingo
+            </Link>
           </h3>
           <div className="lg:flex lg:flex-row-reverse justify-between text-lg w-[30rem] hidden">
-            <Link to="/user">
+            <Link to="/user" aria-label="User Profile">
               <UserCircle2 className="w-8 h-8" />
             </Link>
-            <a href="/" onClick={handleLogOut}>
+            <a href="/" onClick={handleLogOut} aria-label="Logout">
               Logout
             </a>
-            <Link to="/quiz">Quiz</Link>
-            <Link to="/leaderboard">LeaderBoard</Link>
-            {/* <Link to="/add-question">Add Questions</Link> */}
+            <Link to="/quiz" aria-label="Quiz">
+              Quiz
+            </Link>
+            <Link to="/leaderboard" aria-label="Leaderboard">
+              LeaderBoard
+            </Link>
           </div>
-          <button className="lg:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+          <button
+            className="lg:hidden"
+            onClick={() => setMenuOpen(!menuOpen)}
+            aria-label={menuOpen ? "Close Menu" : "Open Menu"}
+          >
             <MenuIcon className="text-white h-6 w-6 cursor-pointer" />
           </button>
         </div>
@@ -43,15 +59,14 @@ const Navbar = () => {
           <button className="lg:hidden" onClick={() => setMenuOpen(!menuOpen)}>
             <X className="text-white h-6 w-6 cursor-pointer" />
           </button>
-          <Link to="/user">
+          <Link to="/user" onClick={closeMenu}>
             <UserCircle2 className="w-20 h-20" />
           </Link>
           <a href="/" onClick={handleLogOut}>
             Logout
           </a>
-          <Link to="/quiz">Quiz</Link>
-          <Link to="/leaderboard">Leader-Board</Link>
-          {/* <Link to="/add-question">Add Questions</Link> */}
+          <Link to="/quiz" onClick={closeMenu}>Quiz</Link>
+          <Link to="/leaderboard" onClick={closeMenu}>Leader-Board</Link>
         </div>
       </div>
     </>
